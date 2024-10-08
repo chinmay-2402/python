11ec7c06e3a4c69b5da9998fdbb5fe5e50e4b778
chinmay-2402 <chavanchinmay2424@gmail.com>
2024-10-08 10:10:37 +0530

    1

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return jsonify({'message': 'Welcome to my Flask App!'})
+
+# Simple API route
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'name': 'Sample Data',
+        'info': 'This is a sample API endpoint.'
+    }
+    return jsonify(data)
+
+# Route to accept POST request
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    data = request.get_json()
+    return jsonify({'received': data})
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
